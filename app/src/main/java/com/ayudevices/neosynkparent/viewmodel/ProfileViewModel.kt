880ea043f5157e120e827cf2aed3389c7574f8b3
Naitik-ag <agarwalnaitikslg@gmail.com>
2025-05-12 16:05:04 +0530

    WebRTC done

--- app/src/main/java/com/ayudevices/neosynkparent/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/ayudevices/neosynkparent/viewmodel/ProfileViewModel.kt
@@ -15,17 +15,18 @@ import com.google.firebase.auth.FirebaseAuth
 @HiltViewModel
 class ProfileViewModel @Inject constructor() : ViewModel() {
     var name by mutableStateOf("")
-    var Loc by mutableStateOf("")
+    var loc by mutableStateOf("")
     var gender by mutableStateOf("")
 
     private val database = FirebaseDatabase.getInstance()
-    private val userRef = database.getReference("NeoSynk")
+    private val userRef = database.getReference("NeoSynk").child("user")
 
     fun saveUserProfile() {
         val userId = FirebaseAuth.getInstance().currentUser?.uid ?: return
         val userProfile = mapOf(
+            "id" to userId,
             "name" to name,
-            "location" to Loc,
+            "location" to loc,
             "gender" to gender
         )
 
@@ -37,4 +38,20 @@ class ProfileViewModel @Inject constructor() : ViewModel() {
                 // Handle error
             }
     }
+
+    fun fetchUserProfile() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid ?: return
+        userRef.child(userId).get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    name = dataSnapshot.child("name").getValue(String::class.java) ?: ""
+                    loc = dataSnapshot.child("location").getValue(String::class.java) ?: ""
+                    gender = dataSnapshot.child("gender").getValue(String::class.java) ?: ""
+                }
+            }
+            .addOnFailureListener {
+                // Handle fetch error
+            }
+    }
 }
+
