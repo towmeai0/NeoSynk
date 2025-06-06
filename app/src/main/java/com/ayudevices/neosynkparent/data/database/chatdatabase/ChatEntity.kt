3697da8d4690780da25e19e277ed2ddc14cc96e4
Naitik-ag <agarwalnaitikslg@gmail.com>
2025-06-06 16:33:11 +0530

    video player done

--- app/src/main/java/com/ayudevices/neosynkparent/data/database/chatdatabase/ChatEntity.kt
+++ app/src/main/java/com/ayudevices/neosynkparent/data/database/chatdatabase/ChatEntity.kt
@@ -1,17 +1,20 @@
 package com.ayudevices.neosynkparent.data.database.chatdatabase
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
+import kotlinx.parcelize.Parcelize
 
-
+@Parcelize
 @Entity(tableName = "chat_messages")
 @TypeConverters(OptionsTypeConverter::class)
 data class ChatEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val message: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    var message: String,
     val sender: String,
     val timestamp: Long = System.currentTimeMillis(),
     val options: List<String> = emptyList(), // For Yes/No/Skip buttons
     val isAnswered: Boolean = false // Flag to track if options have been responded to
-)
+) : Parcelable
